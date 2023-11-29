298828155ca2a92aed5e61e70a6a8f170b798b1a
Dominionss <qshieldt@gmail.com>
2023-11-30 00:32:08 +0200

    Trafic Light

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import pygame
+from trafic_light import TraficLight
 
 pygame.init()
 
+WIDTH, HEIGHT = 768, 768
 
+MAP = pygame.image.load("images/crossroads.jpg")
+MAP = pygame.transform.scale(MAP, (WIDTH, HEIGHT))
 
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+TrafficLights = []
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+
+    pygame.display.update()
+    screen.blit(MAP, (0, 0))
 
---
+++ trafic_light.py
@@ -0,0 +1,6 @@
+import pygame
+
+
+class TraficLight:
+    def __init__(self):
+        pass
