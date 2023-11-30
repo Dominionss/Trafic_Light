53e4a6110a326d845f253d33232ab54a347ffe0c
Dominionss <qshieldt@gmail.com>
2023-11-30 16:37:31 +0200

    Collide point of mouse and traffic lights

--- main.py
+++ main.py
@@ -16,10 +16,21 @@ traffic_light_3 = TrafficLight(100, 450)
 traffic_light_4 = TrafficLight(460, 500)
 TrafficLights = [traffic_light_1, traffic_light_2, traffic_light_3, traffic_light_4]
 
+dragging = False
+selected_TL = None
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if event.button == 1:
+                for traffic_light in TrafficLights:
+                    if traffic_light.hitbox.collidepoint(mouse_x, mouse_y):
+                        selected_TL = traffic_light
+                        dragging = True
+                        traffic_light.hitboxColor = "green"
 
     pygame.display.update()
     screen.blit(MAP, (0, 0))
--- traffic_light.py
+++ traffic_light.py
@@ -9,8 +9,11 @@ class TrafficLight:
         self.height = 153
         self.image = pygame.image.load("images/light/traffic_light.png")
         self.image = pygame.transform.scale(self.image, (self.width, self.height))
-        self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.propertyOfHitbox = {"x1": self.x + 37, "y1": self.y + 20, "x2": self.width - 74, "y2": self.height - 50}
+        self.hitbox = pygame.Rect(self.propertyOfHitbox["x1"], self.propertyOfHitbox["y1"], self.propertyOfHitbox["x2"], self.propertyOfHitbox["y2"])
+        self.hitboxColor = "purple"
 
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
-        pygame.draw.rect(screen, "purple", self.hitbox, 2)
+        pygame.draw.rect(screen, self.hitboxColor, self.hitbox, 2)
+
